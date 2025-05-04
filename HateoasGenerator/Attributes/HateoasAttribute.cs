a499144166680d94e21a37d4d75bfa984f275c40
Lucas <luisernestlucas@gmail.com>
2025-05-04 23:52:44 +0100

    feat: Add new attribute to support actions defined in record classes

---
+++ HateoasGenerator/Attributes/HateoasAttribute.cs
@@ -0,0 +1,30 @@
+﻿using HateoasGenerator.Helpers;
+using Microsoft.CodeAnalysis;
+
+namespace HateoasGenerator.Attributes;
+internal static class HateoasAttribute
+{
+    internal const string FileName = "HateoasAttribute";
+    private const string Class = @"
+namespace HateoasLib.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class HateoasAttribute : Attribute
+{
+    public string Method { get; }
+    public string Relation { get; }
+    public string Property { get; }
+
+    public HateoasAttribute(string method, string relation, string property)
+    {
+        Method = method;
+        Relation = relation;
+        Property = property;
+    }
+}";
+
+    internal static IncrementalGeneratorInitializationContext AddHateoasAttributeToSource(this IncrementalGeneratorInitializationContext context)
+    {
+        return context.AddFileToSource(Class, FileName);
+    }
+}
